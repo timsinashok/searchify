a476254b0ad53297889514aa51cf8045fe46b205
Ashok Timsina <ashoktimsina147181@gmail.com>
2023-09-20 11:00:00 +0400

    updated searchrestult class

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+
+class SearchResult{
+    private:
+    string searchPrompt;
+    struct published_time; 
+
+    public:
+    SearchResult(string searchPrompt);
+
+};
+
+SearchResult::SearchResult(string searchPrompt){
+    
+}
