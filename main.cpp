c671ef1b109d93b8a95101cae7dc2bc5ba5bbeb1
Ashok Timsina <ashoktimsina147181@gmail.com>
2023-10-08 01:46:57 +0400

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,37 @@
 
 using namespace std;
 
+class SearchEngine(){
+    private: 
+    string name;
+    Database database;
+
+}
+
+
+// base class Item 
+class Item{
+    private:
+    int id;
+    string name;
+
+    public:
+    Item(){
+
+    }
+    void setID(){
+        id =  
+    }
+    int getID();
+    string getName();
+};
+
+int Item::getID(){ return id;
+}
+
+string Item::getName(){ return name; }
+
+
 
 class SearchResult{
     private:
@@ -13,8 +44,34 @@ class SearchResult{
     public:
     SearchResult(string searchPrompt);
 
+    // operator overloading to display the results;
+
 };
 
 SearchResult::SearchResult(string searchPrompt){
     
+}
+
+
+class Document{
+    public:
+    private:
+};
+
+
+
+
+
+class SearchDatabase{
+    private:
+    
+
+    public:
+    SearchDatabase(){};
+    void addDocument(Document D);
+};
+
+
+int main(){
+    SearchDatabase 
 }
